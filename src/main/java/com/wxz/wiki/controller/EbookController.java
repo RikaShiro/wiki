00be822c2705c039c8a3d1a734217fd52967b899
RikaShiro <lhwawhite@gmail.com>
2023-03-07 16:15:09 +0800

    a3-6 dd common response

--- src/main/java/com/wxz/wiki/controller/EbookController.java
+++ src/main/java/com/wxz/wiki/controller/EbookController.java
@@ -1,4 +1,5 @@
 package com.wxz.wiki.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,8 @@ import com.wxz.wiki.service.EbookService;
 import jakarta.annotation.Resource;
 
 import com.wxz.wiki.domain.Ebook;
+import com.wxz.wiki.resp.CommonResp;
+
 import java.util.List;
 
 @RestController
@@ -16,8 +19,12 @@ public class EbookController {
 
   @Resource
   private EbookService ebookService;
+
   @GetMapping("/list")
-  public List<Ebook> list() {
-    return ebookService.list();
+  public CommonResp<List<Ebook>> list() {
+    CommonResp<List<Ebook>> resp = new CommonResp<>();
+    List<Ebook> list = ebookService.list();
+    resp.setContent(list);
+    return resp;
   }
 }
