72f312068d5bf8d6ecb83989bff61aff52079800
RikaShiro <lhwawhite@gmail.com>
2023-03-08 17:19:13 +0800

    5-6 axios interceptor

--- web/src/main.ts
+++ web/src/main.ts
@@ -8,6 +8,24 @@ import 'ant-design-vue/dist/antd.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = import.meta.env.VITE_SERVER
+axios.interceptors.request.use(
+  (config) => {
+    console.log('request parameter', config)
+    return config
+  },
+  (err) => {
+    return Promise.reject(err)
+  }
+)
+axios.interceptors.response.use(
+  (res) => {
+    console.log('response', res)
+    return res
+  },
+  (err) => {
+    return Promise.reject(err)
+  }
+)
 
 const app = createApp(App)
 app.use(Antd).use(router)
